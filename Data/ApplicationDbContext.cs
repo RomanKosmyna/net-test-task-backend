40e2f2432ecfc0c674bc43362c0c0324f7a3cd5e
RomanKosmyna <roma.kosmyna@gmail.com>
2024-06-11 13:12:40 +0300

    Setup project. Configure database connection and app database context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using programming_skills_assessment_backend.Models;
+
+namespace programming_skills_assessment_backend.Data;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser>
+{
+    public ApplicationDbContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        List<IdentityRole> roles =
+        [
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            }
+        ];
+
+        builder.Entity<IdentityRole>().HasData(roles);
+    }
+}
